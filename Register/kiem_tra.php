c54a2ffafafd2eae0869b40d32167eeffeacec10
Mtuwn <Trantuan16042003@gmail.com>
2023-09-26 13:42:29 +0700

    update

--- Register/kiem_tra.php
+++ Register/kiem_tra.php
@@ -1,28 +1,34 @@
-        <?php
-        $_name = $_POST['username'];
-        $_password = $_POST['password'];
+<?php
+    
+    $_name = $_POST['username'];
+    $_password = $_POST['password'];
 
-        $ket_noi = mysqli_connect('localhost', 'root', '', 'data');
-        mysqli_set_charset($ket_noi, 'utf8');
-        $sql = "select Name from dang_ky where user = '$_name' and Password = '$_password'";
-        $ket_qua = mysqli_query($ket_noi, $sql);
-        $result = mysqli_fetch_array($ket_qua);
-        print_r($result);
-        if (!empty($result)) {
-                echo "Đăng nhập thành công";
+    //Kết nối database
+    $ket_noi = mysqli_connect('localhost', 'root', '', 'data');
+    mysqli_set_charset($ket_noi, 'utf8');
 
-                session_start();
-                $_SESSION['user'] = $result['Name'];
-     
-                header('location:../Includes/index.php');
-        } else {
-                header("location:log_in.php?error='Sai tài khoản hoặc mật khẩu'");
-        }
+    // Tạo chỗ chống cho dữ liệu người dùng, giúp tách câu lệnh sql khỏi đầu vào dữ liệu
+    $sql = "SELECT Name FROM dang_ky WHERE user = ? AND Password = ?";
+    $stmt = mysqli_prepare($ket_noi, $sql);
 
+    // Liên kết dữ liệu đầu vào và câu lệnh đã chuẩn bị
+    mysqli_stmt_bind_param($stmt, "ss", $_name, $_password);
 
+    // Thực thi
+    mysqli_stmt_execute($stmt);
+    $result = $stmt->get_result();
+    $result_name = $result->fetch_assoc();
 
+    if (!empty($result_name)) {
+        echo "Đăng nhập thành công";
 
-        mysqli_close($ket_noi);
-        ?>
-        
-     
+        session_start();
+        $_SESSION['user'] = $result_name;
+        header('location:../Includes/index.php');
+    } else {
+        header("location:log_in.php?error='Sai tài khoản hoặc mật khẩu'");
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($ket_noi);
+?>
